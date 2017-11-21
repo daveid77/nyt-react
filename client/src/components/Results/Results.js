2ad3ae08f9af5ceea2aae190ba426d4af68f057b
david morse <davidbryanmarshall@gmail.com>
2017-11-20 22:38:07 -0800

    update article and annotation layouts

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -5,8 +5,19 @@ const Results = () =>
     <div class="row">
       <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-default">
-          <div class="panel-body">
+          <div class="panel-heading">
             <h1>Results</h1>
+          </div>
+          <div class="panel-body">
+            
+            <button class="btn btn-md btn-primary pull-right change-saved" data-id="">Save Article</button>
+
+            <h4>Article Title</h4>
+
+            {/*<p>
+              <a href="#" target="_blank">Read More</a>
+            </p>*/}
+
           </div>
         </div>
       </div>
--- client/src/components/Saved/Saved.js
+++ client/src/components/Saved/Saved.js
@@ -5,8 +5,34 @@ const Saved = () =>
     <div class="row">
       <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-default">
-          <div class="panel-body">
+          <div class="panel-heading">
             <h1>Saved</h1>
+          </div>
+          <div class="panel-body">
+
+            <div class="panel panel-default" style={{marginBottom: '5px'}}>
+              <div class="panel-body">
+                
+                <button class="btn btn-md btn-primary pull-right change-saved" data-id="">Remove</button>
+
+                <h4>Article Title</h4>
+                <p>Date Saved: 10/14/17</p>
+
+                {/*<p>
+                  <a href="#" target="_blank">Read More</a>
+                </p>*/}
+
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-body">
+
+                <p>This article blew my mind!</p>
+
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
