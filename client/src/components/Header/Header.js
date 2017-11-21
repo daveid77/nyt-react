75f3934a3d3abfb4cbf5c30178a6439411a6ace1
david morse <davidbryanmarshall@gmail.com>
2017-11-20 22:00:02 -0800

    swap Nav for Header

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () =>
+  <div class="page-header text-center ">
+   <h1>
+      New York Times Article Scrubber<br/>
+      <small>Search for and annotate articles of interest!</small>
+   </h1>
+   
+</div>;
+
+export default Header;
--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,14 +1,14 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "../Nav";
+import Header from "../Header";
 import Search from "../Search";
 import Results from "../Results";
 import Saved from "../Saved";
 
 const App = () =>
   <div>
-    <Nav />
-    <div >
+    <Header />
+    <div>
       <Search />
       <Results />
       <Saved />
